f13fe4822f1116f1d463131fb5eab5d44da3d8fb
mgarmashov <mikhail.garmashov@gmail.com>
2018-09-11 18:09:04 +0500

    deleted collapse in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light"><a class="navbar-brand" href="{{ route('index') }}">{{ config('app.name', '3000 Words') }}</a>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             {{--<li class="nav-item active"><a class="nav-link" href="#">Все слова<span class="sr-only">(current)</span></a></li>--}}
             {{--<li class="nav-item"><a class="nav-link" href="#">Только отмеченные<span class="sr-only">(current)</span></a></li>--}}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
