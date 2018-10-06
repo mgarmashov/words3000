ec98fa41f063f16f21efc056ffb2e120909b417f
Mikhail Garmashov <mikhail.garmashov@gmail.com>
2018-10-06 17:21:00 +0500

    added footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -15,40 +15,23 @@
 @yield('content')
 
 
+<footer>
+    <div class="card">
+        <div class="card-header">
+            Word3000
+        </div>
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <p>"Learn the most important things"</p>
+                <footer class="blockquote-footer">Created By <cite title="Mikhail Garmashov">Mikhail Garmashov</cite></footer>
+            </blockquote>
+        </div>
+    </div>
+</footer>
 <script src="{{ asset('js/main.js') }}"></script>
-<script>
-    $('.btn-favorite').click(function (event) {
-        event.preventDefault();
-        var button = $(this);
-        if($(this).hasClass('active')){
-            console.log('is active');
-            $.get({
-                url: "{{ route('unset-favorite') }}",
-                data: {
-                    word_id: button.closest('.word__item').attr('data-id')
-                },
-                success: function (data) {
-                    button.removeClass('active');
-                }
-            });
 
-        } else {
+@stack('scripts')
 
-            $.get({
-                url: "{{ route('set-favorite') }}",
-                data: {
-                    word_id: button.closest('.word__item').attr('data-id')
-                },
-                success: function (data) {
-                    button.addClass('active');
-                }
-            });
-
-        }
-
-
-    });
-</script>
 </body>
 </html>
 
