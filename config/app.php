3ba61f89ab6a8ec53607195f37d14557d37fc7d0
Mikhail Garmashov <mikhail.garmashov@gmail.com>
2018-12-31 17:29:57 +0500

    added laroute

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Lord\Laroute\LarouteServiceProvider::class,
 
         /*
          * Package Service Providers...
